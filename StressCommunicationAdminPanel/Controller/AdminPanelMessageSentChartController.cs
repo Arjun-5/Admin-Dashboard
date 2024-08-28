4e6fb26f27f22118e5fd5bc3f1547633211bd12b
Rahul <rahul.thangaraj@breakpoint.one>
2024-08-28 18:05:03 +0200

    Refactoring after Demo setup - Wip

---
+++ StressCommunicationAdminPanel/Controller/AdminPanelMessageSentChartController.cs
@@ -0,0 +1,90 @@
+﻿using LiveChartsCore.SkiaSharpView.Painting;
+using LiveChartsCore.SkiaSharpView;
+using SkiaSharp;
+using StressCommunicationAdminPanel.Models;
+using StressCommunicationAdminPanel.Services;
+using System;
+using System.Collections.ObjectModel;
+
+namespace StressCommunicationAdminPanel.Controller
+{
+  public class AdminPanelMessageSentChartController : PropertyChangeHandler
+  {
+    private ObservableCollection<PieSeries<int>> _stressEffectMessagesSeriesCollection;
+
+    public ObservableCollection<PieSeries<int>> StressEffectMessagesSeriesCollection
+    {
+      get => _stressEffectMessagesSeriesCollection;
+
+      set
+      {
+        _stressEffectMessagesSeriesCollection = value;
+
+        OnPropertyChanged(nameof(StressEffectMessagesSeriesCollection));
+      }
+    }
+
+    public AdminPanelMessageSentChartController() 
+    {
+      StressEffectMessagesSeriesCollection = new ObservableCollection<PieSeries<int>>
+      {
+        new PieSeries<int>
+        {
+          Name = Enum.GetName(StressEffectCategory.None),
+          Values = new ObservableCollection<int> { 1 },
+          Stroke = new SolidColorPaint(SKColor.Parse("#414868")) { StrokeThickness = 3 },
+          Fill = new SolidColorPaint(SKColor.Parse("#1a1b26")),
+          DataLabelsPaint = new SolidColorPaint
+          {
+            FontFamily = "Perpetua",
+            SKFontStyle = new SKFontStyle(SKFontStyleWeight.ExtraBold, SKFontStyleWidth.Normal, SKFontStyleSlant.Italic),
+            Color = SKColors.White
+          }
+        }
+      };
+    }
+    private void InitializeMessageSentPieChartSeries()
+    {
+      foreach (StressEffectCategory type in Enum.GetValues(typeof(StressEffectCategory)))
+      {
+        StressEffectMessagesSeriesCollection.Add(new PieSeries<int>
+        {
+          Name = type.ToString(),
+          Values = new ObservableCollection<int> { 0 },
+          Fill = GetColorForStressType(type),
+          DataLabelsSize = 22,
+          Stroke = new SolidColorPaint(SKColor.Parse("#a9b1d6")) { StrokeThickness = 3 },
+          DataLabelsPosition = LiveChartsCore.Measure.PolarLabelsPosition.Middle,
+          DataLabelsPaint = new SolidColorPaint
+          {
+            FontFamily = "Perpetua",
+            SKFontStyle = new SKFontStyle(SKFontStyleWeight.ExtraBold, SKFontStyleWidth.Normal, SKFontStyleSlant.Italic),
+            Color = SKColors.White
+          }
+        });
+      }
+    }
+    private SolidColorPaint GetColorForStressType(StressEffectCategory type)
+    {
+      switch (type)
+      {
+        case StressEffectCategory.Mental:
+          {
+            return new SolidColorPaint(SKColor.Parse("#f7768e"));
+          }
+        case StressEffectCategory.Emotional:
+          {
+            return new SolidColorPaint(SKColor.Parse("#9ece6a"));
+          }
+        case StressEffectCategory.Physical:
+          {
+            return new SolidColorPaint(SKColor.Parse("#2ac3de"));
+          }
+        default:
+          {
+            return new SolidColorPaint(SKColor.Parse("#1a1b26"));
+          }
+      }
+    }
+  }
+}
---
+++ StressCommunicationAdminPanel/Interfaces/IStressPanelChart.cs
@@ -0,0 +1,16 @@
+﻿using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StressCommunicationAdminPanel.Interfaces
+{
+  public interface IStressPanelChart
+  {
+    public void InitializeChartSeries();
+  }
+}
